7cb59fec75e353be5ab80cb79ad8be8a4c6f52f2
Christopher Eagan <ceagan@gmail.com>
2015-08-21 16:53:39 -0400

    Install PostGIS version of PostgreSQL.

--- update.sh
+++ update.sh
@@ -14,12 +14,17 @@ packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
 curl -sSL "${packagesUrl}.bz2" | bunzip2 > "$packages"
 
 for version in "${versions[@]}"; do
-	fullVersion="$(grep -m1 -A10 "^Package: postgresql-$version\$" "$packages" | grep -m1 '^Version: ' | cut -d' ' -f2)"
+	splitPos=$(expr index "$version" '-')
+	pgVersion=${version:0:$(($splitPos - 1))}
+	pgFullVersion="$(grep -m1 -A10 "^Package: postgresql-$pgVersion\$" "$packages" | grep -m1 '^Version: ' | cut -d' ' -f2)"
+	pgisVersion=${version:$splitPos}
+	pgisFullVersion="$(grep -m1 -A10 "^Package: postgresql-$pgVersion-postgis-$pgisVersion\$" "$packages" | grep -m1 '^Version: ' | cut -d' ' -f2)"
 	(
 		set -x
 		cp docker-entrypoint.sh Dockerfile.template "$version/"
 		mv "$version/Dockerfile.template" "$version/Dockerfile"
-		sed -i 's/%%PG_MAJOR%%/'$version'/g; s/%%PG_VERSION%%/'$fullVersion'/g' "$version/Dockerfile"
+		sed -i 's/%%PG_MAJOR%%/'$pgVersion'/g; s/%%PG_VERSION%%/'$pgFullVersion'/g' "$version/Dockerfile"
+		sed -i 's/%%PGIS_MAJOR%%/'$pgisVersion'/g; s/%%PGIS_VERSION%%/'$pgisFullVersion'/g' "$version/Dockerfile"
 	)
 done
 
